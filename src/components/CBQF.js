cc9ca27580d7980157c6db28743e093093e7c839
JohanRomeroMar <jsromero10@ucatolica.edu.co>
2021-12-07 14:06:30 -0500

    cambios CBQ -ALEX

--- src/components/CBQF.js
+++ src/components/CBQF.js
@@ -13,6 +13,15 @@ const CBQF = () => {
     const [CBQ6, setCBQ6] = useState()
     const [CBQ7, setCBQ7] = useState()
     const [CBQ8, setCBQ8] = useState()
+    const [CBQ9, setCBQ9] = useState()
+    const [CBQ10, setCBQ10] = useState()
+    const [CBQ11, setCBQ11] = useState()
+    const [CBQ12, setCBQ12] = useState()
+    const [CBQ13, setCBQ13] = useState()
+    const [CBQ14, setCBQ14] = useState()
+    const [CBQ15, setCBQ15] = useState()
+    const [CBQ16, setCBQ16] = useState()
+    const [CBQ17, setCBQ17] = useState()
 
     const history = useHistory()
 
@@ -45,53 +54,194 @@ const CBQF = () => {
                     <div class="title2">CBQ</div>
                     <div class="content">
                         <form onSubmit={handleSubmit}>
-                            <div class="user-details">
-                                <div class="input-box">
-                                    <span class="details">pregunta1</span>
-                                    <input type="text" placeholder="Ingresa tus nombres" onChange={(e) => { setCBQ1(e.target.value) }} value={CBQ1} required></input>
+                             <div>
+                            <div class="container">
+                                <div class="title">CBQ</div>
+                                <div class="question-container">
+                                    <div class="question-title">1. Mantener peleas y discusiones “online”, empleando insultos, etc. por medio de
+                                        mensajes electrónicos.
+                                    </div>
+                                    <div class="question">
+                                        <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ1(e.target.value) }} required>
+                                            <option selected>Seleccione una opcion</option>
+                                            <option value="1">Nunca</option>
+                                            <option value="2">Alguna vez</option>
+                                            <option value="3">A menudo</option>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div class="input-box">
-                                    <span class="details">pregunta2</span>
-                                    <input type="text" placeholder="Ingresa tus apellidos" onChange={(e) => { setCBQ2(e.target.value) }} required></input>
-                                </div>
-                                <div class="input-box">
-                                    <span class="details">pregunta3</span>
-                                    <select required onChange={(e) => { setCBQ3(e.target.value) }}>
-                                        <option selected value="">Tipo de documento</option>
-                                        <option value="1">Tarjeta de identidad</option>
-                                        <option value="2">Cedula de ciudania</option>
-                                        <option value="3">Pasaporte</option>
+                                <div class="question-container">
+                                    <div class="question-title">2. Enviar mensajes amenazantes o insultantes por e-mail</div>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ2(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
                                     </select>
                                 </div>
-                                <div class="input-box">
-                                    <span class="details">pregunta4</span>
-                                    <input type="text" placeholder="Ingresa tu documento" onChange={(e) => { setCBQ4(e.target.value) }} required></input>
-                                </div>
-                                <div class="input-box">
-                                    <span class="details">pregunta5</span>
-                                    <input type="date" onChange={(e) => { setCBQ5(e.target.value) }} value={CBQ5} required></input>
-                                </div>
-                                <div class="input-box">
-                                    <span class="details">pregunta6</span>
-                                    <input type="email" placeholder="Ingresa tu correo electrónico" onChange={(e) => { setCBQ6(e.target.value) }} required></input>
-                                </div>
-                                <div class="input-box">
-                                    <span class="details">pregunta7</span>
-                                    <input type="text" placeholder="Ingresa tu número de celular" onChange={(e) => { setCBQ7(e.target.value) }} required></input>
-                                </div>
-
-                                <div class="input-box">
-                                    <span class="details">pregunta8</span>
-                                    <select aria-label="Genero" onChange={(e) => { setCBQ8(e.target.value) }} required>
-                                        <option selected value="">Genero</option>
-                                        <option value="1">Masculino</option>
-                                        <option value="2">Femenino</option>
-                                        <option value="3">Prefiero no decirlo</option>
+                                <div class="question-container">
+                                    <div class="question-title">3. Enviar mensajes amenazantes o insultantes por teléfono móvil</div>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ3(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
                                     </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">4. Colgar imágenes en internet de un conocido/a o compañero/a que pueden ser
+                                        humillantes (por ejemplo,
+                                        de
+                                        cuando se estávistiendo en el vestuario del gimnasio). En caso afirmativo describe que tipo de imágenes
+                                    </div>
+                                    <textarea class="question-text-area"></textarea>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ4(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">5. Enviar enlaces de imágenes humillantes a otras personas para que las puedan
+                                        ver. En caso afirmativo
+                                        describe: </div>
+                                    <textarea class="question-text-area"></textarea>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ5(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">6. Escribir bromas, rumores, chismes o comentarios que ponían en ridículo a un
+                                        compañero/a en internet
+                                    </div>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ6(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">7. Enviar enlaces donde aparecían rumores, chismes etc. de un compañero/a o
+                                        conocido/a a otras personas
+                                        para
+                                        que las leyeran. </div>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ7(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">8. Conseguir la contraseña (nicks, claves, etc.) de otra persona y enviar
+                                        mensajes con su nombre por
+                                        e-mail
+                                        que le podían hacer quedar mal ante los demás o crearle problemas con sus conocidos. </div>
+                                    <select class="form-select" aria-label="Default select example" onChange={(e) => { setCBQ8(e.target.value) }} required>
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">9.Grabar video o tomar fotografías con el móvil mientras un grupo se ríe y
+                                        obliga a otra persona a
+                                        hacer
+                                        algo humillante o ridículo En caso afirmativo describe: </div>
+                                    <textarea class="question-text-area"></textarea>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">10.Enviar esas imágenes a otras personas </div>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">11.Grabar video o tomar fotografías con el móvil mientras alguien golpea o hace
+                                        daño a otra persona En
+                                        caso
+                                        afirmativo describe: </div>
+                                    <textarea class="question-text-area"></textarea>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">13.Difundir online secretos, información comprometida o imágenes de alguien.
+                                    </div>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">14.Apartar a alguien de modo intencional de un grupo online (chats, listas de
+                                        amigos, foros temáticos,
+                                        etc.).</div>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">15.Enviar insistentemente (de forma repetida) mensajes que incluyen amenazas o
+                                        que son muy
+                                        intimidatorios.
+                                    </div>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">16.Grabar video o tomar fotografías a algún compañero/a con móvil en algún tipo
+                                        de comportamiento de
+                                        índole
+                                        sexual </div>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="question-container">
+                                    <div class="question-title">17.Enviar esas imágenes a otras personas </div>
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Seleccione una opcion</option>
+                                        <option value="1">Nunca</option>
+                                        <option value="2">Alguna vez</option>
+                                        <option value="3">A menudo</option>
+                                    </select>
+                                </div>
+                                <div class="button">
+                                    <input type="submit" value="Siguiente" onSubmit={handleSubmit}></input>
                                 </div>
                             </div>
-                            <div class="button">
-                                <input type="submit" value="Registrar" onSubmit={handleSubmit}></input>
                             </div>
                         </form>
                     </div>
