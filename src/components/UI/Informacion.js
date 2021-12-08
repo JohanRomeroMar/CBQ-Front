2b75c2632a3ffe657e076af1578933fd1250dfdc
AlexRomero <brayan12r@gmail.com>
2021-12-07 22:05:30 -0500

    se realiza el responsive del modulo perfil

--- src/components/UI/Informacion.js
+++ src/components/UI/Informacion.js
@@ -7,7 +7,42 @@ export const Informacion = () => {
     return (
         <>
             <Navbar/>
-            <h1>Información</h1>
+            <div class="body-register">
+                <div class="container-register">
+                    <div class="title2">Registro de contacto</div>
+                    <div class="content">
+                        <form>
+                            <div class="user-details">
+                                <div class="input-box">
+                                    <span class="details">Asunto</span>
+                                    <input type="text" placeholder="Ingresa tus nombres"  required></input>
+                                </div>
+                                <div class="input-box">
+                                    <span class="details">Apellidos</span>
+                                    <input type="text" placeholder="Ingresa tus apellidos" required></input>
+                                </div>
+                                <div class="input-box">
+                                    <span class="details">Tipo de documento</span>
+                                    <select required >
+                                        <option selected value="">Tipo de documento</option>
+                                        <option value="1">Tarjeta de identidad</option>
+                                        <option value="2">Cedula de ciudania</option>
+                                        <option value="3">Pasaporte</option>
+                                    </select>
+                                </div>
+                                <div class="input-box">
+                                    <span class="details">Documento</span>
+                                    <input type="text" placeholder="Ingresa tu documento" required></input>
+                                </div>
+                            </div>
+                            <div class="button">
+                                {/* <NavLink to={`/CBQ`}></NavLink> */}
+                                <input type="submit" value="Iniciar" ></input>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </>
         
